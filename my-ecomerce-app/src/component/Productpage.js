391b948d491879b51c0975478cd14dae6a6600e9
Alend-M <alend.maci@ucalgary.ca>
2024-03-21 12:52:29 -0600

    Alends work oops not suppose to be in this branch

---
+++ my-ecomerce-app/src/component/Productpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import ProductItem from './ProductItem'; 
+
+const Productpage = () => {
+    return(
+        <div>
+            <Header />
+            <h1>Product Page</h1>
+        </div>
+    );
+}
+
+export default Productpage;
